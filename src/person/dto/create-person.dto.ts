ad2b1c9996c0f83759c2849f7a779d267f2d9710
unknown <emmanuelsehoueto@gmail.com>
2025-08-12 05:18:14 +0100

    set up 'create person' dto

--- src/person/dto/create-person.dto.ts
+++ src/person/dto/create-person.dto.ts
@@ -1,1 +1,19 @@
-export class CreatePersonDto {}
+import { IsEmail, IsNotEmpty, IsString, MinLength } from "class-validator";
+
+export class CreatePersonDto {
+
+    @IsString()
+    @IsNotEmpty()
+    @MinLength(1)
+    firstName: string;
+
+    @IsString()
+    @IsNotEmpty()
+    @MinLength(1)
+    lastName: string;
+
+    @IsString()
+    @IsNotEmpty()
+    @IsEmail()
+    email: string;
+}
